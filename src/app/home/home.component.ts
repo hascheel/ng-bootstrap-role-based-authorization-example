5f3cdb0d58cd4bf7a8e3cc13fa4d621c75f42d45
Hardy Scheel <hardy.scheel@gmail.com>
2020-09-29 18:45:44 +0200

    implement components: admin, home, login

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { first } from 'rxjs/operators';
+
+import { User } from '@app/_models';
+import { UserService, AuthenticationService } from '@app/_services';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  loading = false;
+  user: User;
+  userFromApi: User;
+
+  constructor(
+    private userService: UserService,
+    private authenticationService: AuthenticationService
+  ) {
+    this.user = this.authenticationService.userValue;
+  }
+
+  ngOnInit(): void {
+    this.loading = true;
+    this.userService.getById(this.user.id).pipe(first()).subscribe(user => {
+      this.loading = false;
+      this.userFromApi = user;
+    });
+  }
+
+}
